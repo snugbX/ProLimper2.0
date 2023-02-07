486522b5d963bac1156ecf5b961a2f39e2bc7a84
Everlan Santos <83992078+snugbX@users.noreply.github.com>
2023-02-07 18:20:11 -0300

    Update settings.py

--- Prolimper2/settings.py
+++ Prolimper2/settings.py
@@ -147,5 +147,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-CSRF_TRUSTED_ORIGINS = ['https://prolimper.up.railway.app/','http://prolimper.up.railway.app/']
+if not DEBUG:
+    CSRF_TRUSTED_ORIGINS = ['https://prolimper.up.railway.app','http://prolimper.up.railway.app']
